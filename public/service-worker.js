b028303e31ec45bf917b8c36c43114864f1690a1
RLt <ltfjx2333@gmail.com>
2024-06-28 21:57:23 +0800

    use staticfile cdn

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,22 @@
 
 const static = "dlsf-static-v1"
 const assets = [
-    '/',
-    '/111672633.jpg',
-    '/main.css',
-    '/YUYUKO.otf',
-    '/main.js',
-    '/icon_192.jpg',
-    '/icon_512.jpg'
+    "/",
+    "/111672633.jpg",
+    "/main.css",
+    "/YUYUKO.otf",
+    "/main.js",
+    "/icon_192.jpg",
+    "/icon_512.jpg",
+    "https://cdn.staticfile.net/mdui/2.1.1/mdui.global.js",
+    "https://cdn.staticfile.net/mdui/2.1.1/mdui.css",
+    "https://cdn.staticfile.net/axios/1.6.5/axios.min.js",
+    "https://cdn.staticfile.net/particles.js/2.0.0/particles.min.js",
+    "https://fonts.googleapis.com/icon?family=Material+Icons",
+    "https://fonts.googleapis.com/icon?family=Material+Icons+Outlined",
+    "https://fonts.googleapis.com/icon?family=Material+Icons+Two+Tone",
+    "https://fonts.googleapis.com/icon?family=Material+Icons+Round",
+    "https://fonts.googleapis.com/icon?family=Material+Icons+Sharp"
 ]
 
 self.addEventListener("install", installEvent => {
